f6d42c61235e63b39d5c2d394ca8afb9ae28b4e1
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-17 15:15:29 -0400

    commi schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,12 @@
-CREATE DATABASE burger_db;
-USE burger_db;
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE TABLE burger (
-    id int NOT NULL AUTO_INCREMENT,
-    buerger_name VARCHAR(250) NOT NULL,
-    devoured BOOLEAN DEFAULT false
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int(10) NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(250) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    primary key (id)
 );
+
