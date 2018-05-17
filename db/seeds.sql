61b173cd5fcb3d1eca1718edd21965d8ea698adb
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-17 15:42:55 -0400

    commit revised seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("Cheeseburger", "Bacon Cheeseburger", "Avacado Explosion Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Avacado Explosion Burger");
