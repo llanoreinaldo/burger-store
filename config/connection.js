ab44e848cf16076fbd542ecf5d0b1468b12d9b03
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-18 18:46:16 -0400

    adding heroku mysql dbintegration

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 //Sets up the MYSQL Connection:
-var mysql = require ("mysql");
+var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-host: "localhost",
-port: 3306,
-user: "root",
-password: "Reinaldo33!",
-database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Reinaldo33!",
+        database: "burgers_db"
+    });
+};
 
 //Make a connection
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-        console.error("Error connecting: " +err.stack);
+        console.error("Error connecting: " + err.stack);
         return
     }
     console.log('Connected as id: ' + connection.threadId);
